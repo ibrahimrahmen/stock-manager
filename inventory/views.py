b805d5af4ed371a7aa7c016ae00b1fdc13663eb9
ibrahimrahmen <ibrahimrahmen0@gmail.com>
2026-04-27 16:05:58 +0200

    fix transaction import

--- inventory/views.py
+++ inventory/views.py
@@ -5,6 +5,7 @@ from django.http import JsonResponse, HttpResponseForbidden
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
+from django.db import transaction
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
