206878f05edc41aefe1e755ab7cd0d5ae10ff6f3
ibrahimrahmen <ibrahimrahmen0@gmail.com>
2026-05-04 14:21:43 +0200

    fix email port 465 SSL

--- stock_manager/settings.py
+++ stock_manager/settings.py
@@ -80,8 +80,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'elkamelrami1@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = 'Stock Manager <elkamelrami1@gmail.com>'
